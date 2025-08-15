528ace599ba792ea35e62e80c9215d126400e8d1
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-08-15 19:16:04 +0500

    Completed the dropdown

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./styles.css";
+
+const $dropdownDiv = document.querySelector(".dropdown");
+if ($dropdownDiv) {
+  const $dropdownButton = $dropdownDiv.querySelector("button");
+  if ($dropdownButton) {
+    $dropdownButton.addEventListener("click", function () {
+      $dropdownDiv.classList.toggle("active");
+    });
+  }
+
+  document.addEventListener("click", function (event) {
+    if (!$dropdownDiv.contains(event.target)) {
+      $dropdownDiv.classList.remove("active");
+    }
+  });
+}
