55c3ec5488788bcfd1e6990e2776e525bb81ccd5
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-08-16 13:31:15 +0500

    setup for image carousel

--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
 import "./styles.css";
-
-const $dropdownDiv = document.querySelector(".dropdown");
-if ($dropdownDiv) {
-  const $dropdownButton = $dropdownDiv.querySelector("button");
-  if ($dropdownButton) {
-    $dropdownButton.addEventListener("click", function () {
-      $dropdownDiv.classList.toggle("active");
-    });
-  }
-
-  document.addEventListener("click", function (event) {
-    if (!$dropdownDiv.contains(event.target)) {
-      $dropdownDiv.classList.remove("active");
-    }
-  });
-}
