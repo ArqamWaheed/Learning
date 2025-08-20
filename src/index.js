55d57b52b5cf609b6652e270fc7d173c9ffee246
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-08-20 16:43:58 +0500

    tested with giphy api

--- src/index.js
+++ src/index.js
@@ -1,42 +1,16 @@
 import "./styles.css";
 
-const $leftBtn = document.querySelector(".left");
-const $rightBtn = document.querySelector(".right");
-const $slider = document.querySelector(".wideFrame");
-let activeImage = 1; // Tracking image on display
+const img = document.querySelector("img");
 
-$leftBtn.addEventListener("click", function () {
-  // Leftward movement of carousel
-
-  slideLeft();
-});
-
-$rightBtn.addEventListener("click", function () {
-  // Rightward movement of carousel
-  slideRight();
-});
-
-function slideLeft() {
-  if ($rightBtn.style.display === "none") $rightBtn.style.display = "inline";
-  transformX($slider, "500px");
-  activeImage -= 1;
-  if (activeImage == 1 && $leftBtn.style.display !== "none") {
-    $leftBtn.style.display = "none";
-  }
-}
-
-function slideRight() {
-  if ($leftBtn.style.display === "none") $leftBtn.style.display = "inline";
-  transformX($slider, "-500px");
-  activeImage += 1;
-  if (activeImage == 5 && $rightBtn.style.display !== "none") {
-    $rightBtn.style.display = "none";
-  }
-}
-
-function transformX(element, transformation) {
-  let currentTransform = element.style.transform || "";
-  element.style.transform = `${currentTransform} translateX(${transformation})`;
-}
-
-setInterval(slideRight, 5000);
+fetch(
+  "https://api.giphy.com/v1/gifs/translate?api_key=9DJ3r9jvTt5JDGWaee7k3T1Hg2SO8xtk&s=Test",
+  {
+    mode: "cors",
+  },
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    img.src = response.data.images.original.url;
+  });
