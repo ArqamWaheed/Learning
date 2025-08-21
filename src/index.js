faca26ada52f47a613fbc150097aeef4b555ce2f
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-08-21 17:13:12 +0500

    yo gurt

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import "./styles.css";
 
 const img = document.querySelector("img");
+async function functionAsyncRyan() {
+  const response = await fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=9DJ3r9jvTt5JDGWaee7k3T1Hg2SO8xtk&s=Test",
+    { mode: "cors" },
+  );
+  const data = await response.json();
+  img.src = data.data.images.original.url;
+}
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=9DJ3r9jvTt5JDGWaee7k3T1Hg2SO8xtk&s=Test",
-  {
-    mode: "cors",
-  },
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+functionAsyncRyan();
+
+// .then(function (response) {
+// return response.json();
+// })
+// .then(function (response) {
+// img.src = response.data.images.original.url;
+// });
